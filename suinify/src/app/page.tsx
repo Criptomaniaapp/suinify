9a72b1a1ff76c4a4d8baf55a5b7e7987be045756
criptomaniapp <criptomania.app@gmail.com>
2024-11-20 23:04:03 -0700

    update

--- suinify/src/app/page.tsx
+++ suinify/src/app/page.tsx
@@ -1,9 +1,6 @@
 import Header from '@components/Header';
 
 export default function Page() {
-    return (
-        <div>
-            <Header />
-        </div>
-    );
+  return <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind!</h1>;
 }
+
